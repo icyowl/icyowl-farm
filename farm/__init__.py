fcb17c6f21b496d7d76e32052e38ee2a71dd7668
AkiraWin <frog7bar@gmail.com>
2025-07-22 09:59:53 +0900

    retry

--- farm/__init__.py
+++ farm/__init__.py
@@ -1,21 +1,12 @@
 import os
 from flask import Flask, render_template, redirect, url_for, session
 from .auth import login_required
-import configparser
 
 
 def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     
-    # config = configparser.ConfigParser()
-    # config.read(os.path.abspath(os.path.join(app.instance_path, 'config.ini')))
-
-    # app.config.from_mapping(
-    #     SECRET_KEY = 'dev',
-    #     MONGO_URI = config['DEV']['DB_URI']  # DEV or PROD
-    # )
- 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
         pass
