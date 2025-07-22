c886bfa31081d8334631868f0a08abebcbfd1645
AkiraWin <frog7bar@gmail.com>
2025-07-22 10:47:49 +0900

    re freeze

--- farm/__init__.py
+++ farm/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, url_for, session
-from .auth import login_required
+from farm.auth import login_required
 
 
 def create_app(test_config=None):
@@ -8,10 +8,11 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-        pass
+        app.config.from_pyfile('config.py') # , silent=True)
     else:
         app.config.from_mapping(test_config)
+
+    # print(app.config['MONGO_URI'])
 
     from farm.db import mongo, init_db
     init_db(app)
