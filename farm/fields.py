bc61faf5967ed11692062dc085c5e768853b00b0
AkiraWin <frog7bar@gmail.com>
2025-08-29 20:35:27 +0900

    sort docs

--- farm/fields.py
+++ farm/fields.py
@@ -21,14 +21,17 @@ def documents(idx):
     cur = mongo.db.species.find({'field': i})
     for d in cur:
         s_id = d['_id']
+        s_no = d['sort_no']
         cu = mongo.db.variety.find({'parent': str(s_id)})
         for v in cu:
             v_id = v['_id']
+            v_no = v['sort_no']
+            sort_no = s_no * 100 + v_no
             doc = mongo.db.document.find_one({'parent': str(v_id)})
             variety_id = str(v_id)
             variety = doc['variety']
             text = doc['document']
             # print(text.split('\r\n'))
             excerpt = text.split('\r\n')[0]
-            docs.append((variety_id, variety, excerpt))
-    return render_template('fields.html', idx=idx, docs=docs)
+            docs.append((sort_no, variety_id, variety, excerpt))
+    return render_template('fields.html', idx=idx, docs=sorted(docs))
